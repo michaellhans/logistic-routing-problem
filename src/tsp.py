e1891bf91876b8b927020a0eed8b49b82b5d65c5
michaelhans <michaelhans777@gmail.com>
2020-05-28 16:00:16 +0700

    TSP Completed

--- src/tsp.py
+++ src/tsp.py
@@ -36,17 +36,29 @@ def ReducedCertainColumn(M, j, length, k):
 
 # Check if there are at least one zero in row i
 def CheckZeroInRow(M, i, length):
+    count = 0
     for j in range (length):
         if (M[i][j] == 0):
             return True
-    return False
+        if (M[i][j] == -999):
+            count += 1
+    if (count == length):
+        return True
+    else:
+        return False
 
 # Check if there are at least one zero in column j
 def CheckZeroInColumn(M, j, length):
+    count = 0
     for i in range (length):
         if (M[i][j] == 0):
             return True
-    return False
+        if (M[i][j] == -999):
+            count += 1
+    if (count == length):
+        return True
+    else:
+        return False
 
 # Get Reduced Matrix using Reduced Cost Matrix
 def GetReducedMatrix(M, length):
@@ -59,7 +71,6 @@ def GetReducedMatrix(M, length):
             cost += min
             ReducedCertainRow(MTemp, i, length, min)
 
-    PrintMatrix(MTemp, length)
     # Make at least one zero in every column
     for j in range (length):
         if (not(CheckZeroInColumn(MTemp, j, length))):
@@ -67,17 +78,96 @@ def GetReducedMatrix(M, length):
             cost += min
             ReducedCertainColumn(MTemp, j, length, min)
 
-    PrintMatrix(MTemp, length)
     return MTemp, cost
 
+# Applying three golden rules for reduced cost matrix
+def CalculateCost(M, length, i, j, BaseCity):
+    MTemp = M
+    for k in range (length):
+        MTemp[i][k] = -999
+    for k in range (length):
+        MTemp[k][j] = -999
+    MTemp[j][i] = -999
+    MTemp[j][BaseCity] = -999
+    return GetReducedMatrix(MTemp, length)
+
+# Applying three golden rules for reduced cost matrix
+def ApplyTheRules(M, length, i, j):
+    for k in range (length):
+        M[i][k] = -999
+    for k in range (length):
+        M[k][j] = -999
+    M[j][i] = -999
+
+# Check if all city is visited
+def IsAllVisited(M, length):
+    for i in range(length):
+        for j in range(length):
+            if (M[i][j] != -999):
+                return False
+    return True
+
+# Print route
+def PrintRoute(route):
+    for i in range (len(route)-1):
+        print(route[i],"->", end=" ")
+    print(route[len(route)-1])
+
+# Driver for TSP
 maps = {}
 maps = LoadCoordinate("SthyrelestNode.txt")
 PrintCoordinateInfo(maps)
 streets = LoadStreet("SthyrelestEdge.txt")
 PrintStreetInfo(streets)
-graphMatrix = ConvertStreetsIntoGraph(streets, len(maps))
-PrintMatrix(graphMatrix, len(maps))
+length = len(maps)
+graphMatrix = ConvertStreetsIntoGraph(streets, length)
+PrintMatrix(graphMatrix, length)
 
-MTemp, cost = GetReducedMatrix(graphMatrix, len(maps))
-PrintMatrix(MTemp, len(maps))
-print("Reduced cost =", cost)
+MTemp, rootcost = GetReducedMatrix(graphMatrix, length)
+PrintMatrix(MTemp, length)
+print("Reduced root cost =", rootcost)
+print("Start here")
+# ApplyTheRules(MTemp, length, 0, 1)
+# PrintMatrix(MTemp, length)
+
+routeParent = [0]
+QueueRoute = []
+StartingCity = 0
+BaseCity = StartingCity
+count = 0
+while (not(IsAllVisited(MTemp, length))):
+    for j in range(length):
+        route = routeParent
+        if (MTemp[StartingCity][j] != -999):
+            count += 1
+            temp = MTemp[StartingCity][j]
+            print("Simpul",count,":",StartingCity, "->", j)
+            M, cost = CalculateCost(MTemp, length, StartingCity, j, BaseCity)
+            PrintMatrix(M, length)
+            finalcost = rootcost + temp + cost
+            print("Reduced total cost = "+str(rootcost)+" + "+"M["+str(StartingCity)+","+str(j)+"] + "+str(cost)+" = "+str(finalcost))
+            route.append(j)
+            print("Route for node",count,"==>",end=" ")
+            PrintRoute(route)
+            QueueRoute.append([count, M, finalcost, route])
+            input()
+    QueueRoute.sort()
+
+    if (len(QueueRoute) == 0):
+        print("Rute perjalanan bolak-balik tidak ditemukan!")
+        break
+
+    nextState = QueueRoute.pop()
+    MTemp = nextState[1]
+    StartingCity = nextState[3][-1]
+    routeParent = nextState[3]
+    rootcost = nextState[2]
+
+if (IsAllVisited):
+    print("Rute telah ditemukan!")
+    routeParent.append(BaseCity)
+    print("Rute perjalanan terpendek TSP adalah ",end=" ")
+    PrintRoute(routeParent)
+    print("Jarak tempu perjalanan =", rootcost, "km")
+else:
+    print("Tidak ada rute perjalanan terpendek TSP yang tersedia!")
